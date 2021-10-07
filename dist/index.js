617823450dc64e5a609aff885a3e13636213d84e
Daniel Luca <hey@danielluca.com>
2021-10-07 12:03:07 +0200

    feat: add calculation

---
+++ dist/index.js
@@ -0,0 +1,600 @@
+var fees = [
+    {
+        start: 0,
+        limit: 25000,
+        fee: 40.3
+    },
+    {
+        start: 25001,
+        limit: 30000,
+        fee: 44.6
+    },
+    {
+        start: 30001,
+        limit: 35000,
+        fee: 48.85
+    },
+    {
+        start: 35001,
+        limit: 40000,
+        fee: 53.05
+    },
+    {
+        start: 40001,
+        limit: 45000,
+        fee: 57.3
+    },
+    {
+        start: 45001,
+        limit: 50000,
+        fee: 61.5
+    },
+    {
+        start: 50001,
+        limit: 65000,
+        fee: 66
+    },
+    {
+        start: 65001,
+        limit: 80000,
+        fee: 70.55
+    },
+    {
+        start: 80001,
+        limit: 95000,
+        fee: 75.1
+    },
+    {
+        start: 95001,
+        limit: 110000,
+        fee: 79.65
+    },
+    {
+        start: 110001,
+        limit: 125000,
+        fee: 84.15
+    },
+    {
+        start: 125001,
+        limit: 140000,
+        fee: 88.65
+    },
+    {
+        start: 140001,
+        limit: 155000,
+        fee: 93.2
+    },
+    {
+        start: 155001,
+        limit: 170000,
+        fee: 97.7
+    },
+    {
+        start: 170001,
+        limit: 185000,
+        fee: 102.25
+    },
+    {
+        start: 185001,
+        limit: 200000,
+        fee: 106.8
+    },
+    {
+        start: 200001,
+        limit: 230000,
+        fee: 113.75
+    },
+    {
+        start: 230001,
+        limit: 260000,
+        fee: 120.7
+    },
+    {
+        start: 260001,
+        limit: 290000,
+        fee: 127.6
+    },
+    {
+        start: 290001,
+        limit: 320000,
+        fee: 134.85
+    },
+    {
+        start: 320001,
+        limit: 350000,
+        fee: 138
+    },
+    {
+        start: 350001,
+        limit: 380000,
+        fee: 141.05
+    },
+    {
+        start: 380001,
+        limit: 410000,
+        fee: 144.1
+    },
+    {
+        start: 410001,
+        limit: 440000,
+        fee: 146.95
+    },
+    {
+        start: 440001,
+        limit: 470000,
+        fee: 149.75
+    },
+    {
+        start: 470001,
+        limit: 500000,
+        fee: 152.55
+    },
+    {
+        start: 500001,
+        limit: 550000,
+        fee: 156.6
+    },
+    {
+        start: 550001,
+        limit: 600000,
+        fee: 160.55
+    },
+    {
+        start: 600001,
+        limit: 650000,
+        fee: 167.6
+    },
+    {
+        start: 650001,
+        limit: 700000,
+        fee: 174.65
+    },
+    {
+        start: 700001,
+        limit: 750000,
+        fee: 181.7
+    },
+    {
+        start: 750001,
+        limit: 800000,
+        fee: 188.75
+    },
+    {
+        start: 800001,
+        limit: 850000,
+        fee: 195.8
+    },
+    {
+        start: 850001,
+        limit: 900000,
+        fee: 202.85
+    },
+    {
+        start: 900001,
+        limit: 950000,
+        fee: 209.9
+    },
+    {
+        start: 950001,
+        limit: 1000000,
+        fee: 216.95
+    },
+    {
+        start: 1000001,
+        limit: 1050000,
+        fee: 224
+    },
+    {
+        start: 1050001,
+        limit: 1100000,
+        fee: 231.05
+    },
+    {
+        start: 1100001,
+        limit: 1150000,
+        fee: 238.1
+    },
+    {
+        start: 1150001,
+        limit: 1200000,
+        fee: 245.15
+    },
+    {
+        start: 1200001,
+        limit: 1250000,
+        fee: 252.2
+    },
+    {
+        start: 1250001,
+        limit: 1300000,
+        fee: 259.25
+    },
+    {
+        start: 1300001,
+        limit: 1350000,
+        fee: 266.3
+    },
+    {
+        start: 1350001,
+        limit: 1400000,
+        fee: 273.35
+    },
+    {
+        start: 1400001,
+        limit: 1450000,
+        fee: 280.4
+    },
+    {
+        start: 1450001,
+        limit: 1500000,
+        fee: 287.45
+    },
+    {
+        start: 1500001,
+        limit: 1550000,
+        fee: 294.5
+    },
+    {
+        start: 1550001,
+        limit: 1600000,
+        fee: 301.55
+    },
+    {
+        start: 1600001,
+        limit: 1650000,
+        fee: 308.6
+    },
+    {
+        start: 1650001,
+        limit: 1700000,
+        fee: 315.65
+    },
+    {
+        start: 1700001,
+        limit: 1750000,
+        fee: 322.7
+    },
+    {
+        start: 1750001,
+        limit: 1800000,
+        fee: 329.75
+    },
+    {
+        start: 1800001,
+        limit: 1850000,
+        fee: 336.8
+    },
+    {
+        start: 1850001,
+        limit: 1900000,
+        fee: 343.85
+    },
+    {
+        start: 1900001,
+        limit: 1950000,
+        fee: 350.9
+    },
+    {
+        start: 1950001,
+        limit: 2000000,
+        fee: 357.95
+    },
+    {
+        start: 2000001,
+        limit: 2050000,
+        fee: 365.0
+    },
+    {
+        start: 2050001,
+        limit: 2100000,
+        fee: 372.05
+    },
+    {
+        start: 2100001,
+        limit: 2150000,
+        fee: 379.1
+    },
+    {
+        start: 2150001,
+        limit: 2200000,
+        fee: 386.15
+    },
+    {
+        start: 2200001,
+        limit: 2250000,
+        fee: 393.2
+    },
+    {
+        start: 2250001,
+        limit: 2300000,
+        fee: 400.25
+    },
+    {
+        start: 2300001,
+        limit: 2350000,
+        fee: 407.3
+    },
+    {
+        start: 2350001,
+        limit: 2400000,
+        fee: 414.35
+    },
+    {
+        start: 2400001,
+        limit: 2450000,
+        fee: 421.4
+    },
+    {
+        start: 2450001,
+        limit: 2500000,
+        fee: 428.45
+    },
+    {
+        start: 2500001,
+        limit: 2550000,
+        fee: 435.5
+    },
+    {
+        start: 2550001,
+        limit: 2600000,
+        fee: 442.55
+    },
+    {
+        start: 2600001,
+        limit: 2650000,
+        fee: 449.6
+    },
+    {
+        start: 2650001,
+        limit: 2700000,
+        fee: 456.65
+    },
+    {
+        start: 2700001,
+        limit: 2750000,
+        fee: 463.7
+    },
+    {
+        start: 2750001,
+        limit: 2800000,
+        fee: 470.75
+    },
+    {
+        start: 2800001,
+        limit: 2850000,
+        fee: 477.8
+    },
+    {
+        start: 2850001,
+        limit: 2900000,
+        fee: 484.85
+    },
+    {
+        start: 2900001,
+        limit: 2950000,
+        fee: 491.9
+    },
+    {
+        start: 2950001,
+        limit: 3000000,
+        fee: 498.95
+    },
+    {
+        start: 3000001,
+        limit: 3050000,
+        fee: 506
+    },
+    {
+        start: 3050001,
+        limit: 3100000,
+        fee: 513.05
+    },
+    {
+        start: 3100001,
+        limit: 3150000,
+        fee: 520.1
+    },
+    {
+        start: 3150001,
+        limit: 3200000,
+        fee: 527.15
+    },
+    {
+        start: 3200001,
+        limit: 3250000,
+        fee: 534.2
+    },
+    {
+        start: 3250001,
+        limit: 3300000,
+        fee: 541.25
+    },
+    {
+        start: 3300001,
+        limit: 3350000,
+        fee: 548.3
+    },
+    {
+        start: 3350001,
+        limit: 3400000,
+        fee: 555.35
+    },
+    {
+        start: 3400001,
+        limit: 3450000,
+        fee: 562.4
+    },
+    {
+        start: 3450001,
+        limit: 3500000,
+        fee: 569.45
+    },
+    {
+        start: 3500001,
+        limit: 3550000,
+        fee: 576.5
+    },
+    {
+        start: 3550001,
+        limit: 3600000,
+        fee: 583.55
+    },
+    {
+        start: 3600001,
+        limit: 3650000,
+        fee: 590.6
+    },
+    {
+        start: 3650001,
+        limit: 3700000,
+        fee: 597.65
+    },
+    {
+        start: 3700001,
+        limit: 3750000,
+        fee: 604.7
+    },
+    {
+        start: 3750001,
+        limit: 3800000,
+        fee: 611.75
+    },
+    {
+        start: 3800001,
+        limit: 3850000,
+        fee: 618.8
+    },
+    {
+        start: 3850001,
+        limit: 3900000,
+        fee: 625.85
+    },
+    {
+        start: 3900001,
+        limit: 3950000,
+        fee: 632.9
+    },
+    {
+        start: 3950001,
+        limit: 4000000,
+        fee: 639.95
+    },
+    {
+        start: 4000001,
+        limit: 4050000,
+        fee: 647
+    },
+    {
+        start: 4050001,
+        limit: 4100000,
+        fee: 654.05
+    },
+    {
+        start: 4100001,
+        limit: 4150000,
+        fee: 661.1
+    },
+    {
+        start: 4150001,
+        limit: 4200000,
+        fee: 668.15
+    },
+    {
+        start: 4200001,
+        limit: 4250000,
+        fee: 675.2
+    },
+    {
+        start: 4250001,
+        limit: 4300000,
+        fee: 682.25
+    },
+    {
+        start: 4300001,
+        limit: 4350000,
+        fee: 689.3
+    },
+    {
+        start: 4350001,
+        limit: 4400000,
+        fee: 696.35
+    },
+    {
+        start: 4400001,
+        limit: 4450000,
+        fee: 703.4
+    },
+    {
+        start: 4450001,
+        limit: 4500000,
+        fee: 710.45
+    },
+    {
+        start: 4500001,
+        limit: 4550000,
+        fee: 717.5
+    },
+    {
+        start: 4550001,
+        limit: 4600000,
+        fee: 724.55
+    },
+    {
+        start: 4600001,
+        limit: 4650000,
+        fee: 731.6
+    },
+    {
+        start: 4650001,
+        limit: 4700000,
+        fee: 738.65
+    },
+    {
+        start: 4700001,
+        limit: 4750000,
+        fee: 745.7
+    },
+    {
+        start: 4750001,
+        limit: 4800000,
+        fee: 752.75
+    },
+    {
+        start: 4800001,
+        limit: 4850000,
+        fee: 759.8
+    },
+    {
+        start: 4850001,
+        limit: 4900000,
+        fee: 766.85
+    },
+    {
+        start: 4900001,
+        limit: 4950000,
+        fee: 773.9
+    },
+    {
+        start: 4950001,
+        limit: 5000000,
+        fee: 780.95
+    },
+];
+var formatCurrency = function (value) {
+    return new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR"
+    }).format(value);
+};
+var calculateFees = function (propertyValue, amountOfDeclarations) {
+    var value = 0;
+    fees.forEach(function (entry) {
+        if (propertyValue >= entry.start && propertyValue <= entry.limit) {
+            value = amountOfDeclarations * entry.fee;
+        }
+    });
+    var minValue = formatCurrency(value * 1);
+    var midValue = formatCurrency(value * 9.5);
+    var maxValue = formatCurrency(value * 18);
+    return { minValue: minValue, midValue: midValue, maxValue: maxValue };
+};
